406f0c52dd2695d2f4f3475eeab4ce424530efb5
Elizabethpv <48952600+Elizabethpv@users.noreply.github.com>
2021-03-24 12:36:27 +0530

    Add project files.

---
+++ ViewBagApp/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewBagApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult StudentView()
+        {
+            ViewBag.Name = "Ansu Mathew";
+            ViewBag.Address = "Pookattil House";
+            ViewBag.Email = "ansu@gmail.com";
+            ViewBag.PhoneNumber = "9976785456";
+            ViewBag.College = "Mount Carmel College";
+            ViewBag.Department = "Computer Application";
+            ViewBag.UserName = "ansu123";
+            ViewBag.presentTime = DateTime.Now;
+            return View(); ;
+        }
+    }
+}
